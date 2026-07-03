249573c948e3fd8729f8e5bc6a0b19d4fc5609db
ThuAhn <nthu47200@gmail.com>
2026-07-03 03:05:42 +0000

    clean comments

--- Agent_25127446_25127320_25127151/src/tools/tool.h
+++ Agent_25127446_25127320_25127151/src/tools/tool.h
@@ -3,15 +3,14 @@
 #include <map>
 
 class Tool {
-protected:// chỉ cho lớp con kế thừa sd 
+protected:
     std::string name;
     std::string description;
 
 public:
     Tool(const std::string& n, const std::string& d) : name(n), description(d) {}
-    virtual ~Tool() = default; // Luôn cần destructor ảo cho abstract class
-
-    // Hàm thuần ảo (Pure Virtual Function) - tránh memory leak
+    virtual ~Tool() = default; 
+s
     virtual std::string execute(const std::map<std::string, std::string>& args) = 0;
 
     std::string getName() const { return name; }
