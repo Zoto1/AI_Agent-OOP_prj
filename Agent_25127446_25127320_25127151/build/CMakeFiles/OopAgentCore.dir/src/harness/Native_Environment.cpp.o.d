005dac259ed0405ef58711dfef3aa2897be493b0
sand012 <hnhatha70@gmail.com>
2026-07-30 01:47:52 +0700

    Remove build artifacts from git tracking

--- Agent_25127446_25127320_25127151/build/CMakeFiles/OopAgentCore.dir/src/harness/Native_Environment.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/OopAgentCore.dir/src/harness/Native_Environment.cpp.o: \
- /workspaces/AI_Agent-OOP_prj/Agent_25127446_25127320_25127151/src/harness/Native_Environment.cpp \
- /usr/include/stdc-predef.h
